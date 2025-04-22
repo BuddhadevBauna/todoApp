95f2d5ea8c37751aa2bc399597ee8a41fccc2b2d
Buddhadev Bauna <baunabuddhadev2001@gmail.com>
2025-04-22 20:30:18 +0530

    fixed

--- client/src/store/context/auth-context.jsx
+++ client/src/store/context/auth-context.jsx
@@ -31,7 +31,6 @@ export const AuthProvider = ({ children }) => {
                     setLoginUserData(response.data);
                 }
             } catch (error) {
-                console.log(error);
                 setLogIn(false);
                 setLoginUserData(null);
             } finally {
