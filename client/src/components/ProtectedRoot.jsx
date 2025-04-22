3cbff9122b93490f93047e54e09c00efc7284586
Buddhadev Bauna <baunabuddhadev2001@gmail.com>
2025-04-23 01:10:51 +0530

    issue resolve

--- client/src/components/ProtectedRoot.jsx
+++ client/src/components/ProtectedRoot.jsx
@@ -5,7 +5,7 @@ const ProtectedRoot = ({ children }) => {
     const { isLogIn, isLoadingUserData } = useAuth();
 
     if (isLoadingUserData) return <p>Loading...</p>;
-    if (!isLogIn) {
+    else if (!isLogIn) {
         return (
             <div className="flex flex-col items-center justify-center h-[calc(100vh-80px)] text-center px-4">
                 <p className="text-xl font-semibold text-red-600 mb-4">
